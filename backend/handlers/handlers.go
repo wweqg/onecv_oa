27a575e3d0f22086900268a37986a29b8e2b2d79
wweqg <e0726629@u.nus.edu>
2023-09-25 18:32:45 +0800

    impl suspend endpoint

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -111,3 +111,80 @@ func GetCommonStudents(c *fiber.Ctx) error {
 	})
 }
 
+func SuspendStudent(c *fiber.Ctx) error {
+	db := database.DB.Db
+
+	var request struct {
+		Student string `json:"student"`
+	}
+
+	if err := c.BodyParser(&request); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid request payload",
+		})
+	}
+
+	var student models.Student
+	if err := db.Where("email = ?", request.Student).First(&student).Error; err != nil {
+		return c.Status(http.StatusNotFound).JSON(fiber.Map{
+			"message": "Student not found",
+		})
+	}
+
+	// Suspend the student
+	db.Model(&student).Update("suspend", true)
+
+	return c.SendStatus(http.StatusNoContent)
+}
+
+// // RetrieveStudentsForNotifications retrieves a list of students who can receive a given notification.
+// func RetrieveStudentsForNotifications(c *fiber.Ctx) error {
+// 	db := c.Locals("db").(*gorm.DB)
+
+// 	// Define request structure
+// 	var request struct {
+// 		Teacher      string `json:"teacher"`
+// 		Notification string `json:"notification"`
+// 	}
+
+// 	// Parse the request body into the request struct
+// 	if err := c.BodyParser(&request); err != nil {
+// 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"message": "Invalid request payload"})
+// 	}
+
+// 	// Find the teacher by email
+// 	var teacher models.Teacher
+// 	if err := db.Where("email = ?", request.Teacher).First(&teacher).Error; err != nil {
+// 		return c.Status(http.StatusNotFound).JSON(fiber.Map{"message": "Teacher not found"})
+// 	}
+
+// 	// Parse the notification text to extract mentioned student emails
+// 	mentionedStudents := extractMentionedStudents(request.Notification)
+
+// 	// Find the students who can receive the notification
+// 	var recipients []models.Student
+// 	db.Table("students").
+// 		Joins("JOIN teacher_students ON students.id = teacher_students.student_id").
+// 		Joins("JOIN teachers ON teacher_students.teacher_id = teachers.id").
+// 		Where("teachers.email = ?", teacher.Email).
+// 		Where("students.suspend = ?", false).
+// 		Where("students.email IN (?)", mentionedStudents).
+// 		Find(&recipients)
+
+// 	// Extract the recipient student emails
+// 	recipientEmails := []string{}
+// 	for _, recipient := range recipients {
+// 		recipientEmails = append(recipientEmails, recipient.Email)
+// 	}
+
+// 	return c.JSON(fiber.Map{"recipients": recipientEmails})
+// }
+
+// // Helper function to extract mentioned student emails from notification text
+// func extractMentionedStudents(notificationText string) []string {
+// 	// Implement your logic to extract mentioned student emails here
+// 	// For example, you can use regular expressions to find email addresses prefixed with '@'
+// 	// and extract them into a slice.
+// 	// Return the list of mentioned student emails.
+// 	return []string{} // Replace with the actual implementation
+// }
